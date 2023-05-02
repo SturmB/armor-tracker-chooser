13763b939dc18b6f8dd12fa7386b02aef0fcac48
Chris McGee <chris@chrismcgee.info>
2023-05-02 15:05:17 -0400

    🎉 Finishes first draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,13 @@
 export default {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "zelda-sword": "url('/zelda-sword-01.jpg')",
+        "botw": "url('/bg-botw-01.jpg')",
+        "totk": "url('/bg-totk-01.jpg')",
+      },
+    },
   },
   plugins: [],
-}
-
+};
